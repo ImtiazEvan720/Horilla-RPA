2f0422892e24785e13508c578d127728d64fce74
Imtiaz Evan <imtiazevan720@gmail.com>
2024-08-02 11:14:35 -0500

    Updated memory usage celery

--- start_server.sh
+++ start_server.sh
@@ -4,6 +4,10 @@
 DJANGO_SETTINGS_MODULE="myproject.settings"
 DJANGO_APP="horilla"
 
+# Define memory limit and concurrency
+MEMORY_LIMIT_KB=100000  # Memory limit in kilobytes (200 MB)
+CONCURRENCY=2           # Number of worker processes
+
 # Navigate to the project directory (assuming the script is run from the root)
 cd "$(dirname "$0")" || exit
 
@@ -37,6 +41,6 @@ gunicorn --bind 0.0.0.0:80 horilla.wsgi:application &
 
 # Start the Celery worker
 echo "Starting the Celery worker..."
-celery -A $DJANGO_APP worker &
+celery -A $DJANGO_APP worker --loglevel=info --max-memory-per-child=$MEMORY_LIMIT_KB --concurrency=$CONCURRENCY &
 celery -A $DJANGO_APP beat
 
